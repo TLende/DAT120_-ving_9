f469e0bc9890611fde14b7b2531b3a5db8529df7
Thomas <tlende12@live.no>
2022-10-26 22:04:27 +0200

    V0.3

--- klasser.py
+++ klasser.py
@@ -2,7 +2,7 @@ import datetime
 
 class avtale:
     def __init__(self, tittel ="", sted ="", starttidspunkt="", varighet=0):
-        Tittel = str(tittel)
-        Sted = str(sted)
-        Starttidspunkt = datetime.datetime.fromisoformat(starttidspunkt)
-        Varighet = int(varighet)
+        self.Tittel = str(tittel)
+        self.Sted = str(sted)
+        self.Starttidspunkt = datetime.datetime.fromisoformat(starttidspunkt)
+        self.Varighet = int(varighet)
