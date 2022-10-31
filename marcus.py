1fa24a5208ed207e33db1007366b4669f4f87586
MSivertse <marcus.sivertsen99@hotmail.com>
2022-10-31 09:01:19 +0100

    test

---
+++ marcus.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 31 08:41:34 2022
+
+@author: marcu
+"""
+
+djsbvalvnj kzjxcvljnxvnzøxkjnvøkj
+vsdjvbdfznb 
+
+zdfb
+nbzd
+nz
+next(
+     )
+print()
