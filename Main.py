ee9fd829fbb9958b45272d1d0cf9cc60e8c7fcda
CHalexanderokokm <alexander.hvid@icloud.com>
2022-11-01 19:31:21 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,10 +4,8 @@ from datetime import datetime
 
 list = list()
 
+#list.append(klasser.avtale("Skule","uis",datetime.now(),20))
+#list.append(klasser.avtale("Test","Test2",datetime.now(),30))
 
-list.append(klasser.avtale("Skule","uis",datetime.now(),20))
-list.append(klasser.avtale("Test","Test2",datetime.now(),30))
-
-
-
-Funksjoner.utskrift_avtaler(list)
+if __name__ == "__main__":
+    Funksjoner.meny()
