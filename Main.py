f650f6e5a8fff9e3e4c5820dbbb6b05a11141a58
CHalexanderokokm <alexander.hvid@icloud.com>
2022-11-01 19:42:23 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,4 +8,23 @@ list = list()
 #list.append(klasser.avtale("Test","Test2",datetime.now(),30))
 
 if __name__ == "__main__":
-    Funksjoner.meny()
+    while True:
+        menyl = [
+            "   [1]Les avtaler fra fil"
+            "   [2]Skriv avtaler fra fil"
+            "   [3]Skriv inn ny avtale"
+            "   [4]Skriv ut alle avtaler"
+            "   [5]Avslutt"
+        ]
+        menyv = [
+            Funksjoner.utskrift_avtaler(list),
+            Funksjoner.ny_avtale()
+        ]
+        print("Valgene er")
+        for x in range(len(menyl)):
+            print(menyl[x],end='/n')
+        try:
+            menyv[int(input("Skriv inn valget her 1-5: "))]
+        except:
+            pass
+    
