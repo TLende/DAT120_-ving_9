608928ac69e5195bda85ab036a7d1f89b25225c7
MSivertse <marcus.sivertsen99@hotmail.com>
2022-11-09 11:52:52 +0100

    V2.Marcus

--- Oving_10.py
+++ Oving_10.py
@@ -1,1 +1,14 @@
+def nyttsted():
+    return Sted(input("StedID:"), input("Sted navn:"), input("gateadresse:"), int(input("postnr:")), input("poststed:"))
 
+def lage_stedliste(list):
+    doc = open (input("Skriv inn ønsket navn på fil: "), "w", encoding="UTF-8")
+    for i in range(len(list)):
+        temp_str = F"{list[i].ID};{list[i].Tittel};{list[i].gateadresse};{list[i].postnr};{list[i].poststed} \n"
+        doc.write(temp_str)
+    doc.close()
+
+if __name__ == "__main__":
+    liste = list()
+    liste.append (nyttsted())
+    print(liste[0])
--- klasser.py
+++ klasser.py
@@ -13,3 +13,13 @@ class avtale:
 
     def __str__(self):
         return f"Avtale:{self.Tittel}, Sted:{self.Sted}, Tid:{self.Starttidspunkt}, og varer:{self.Varighet} min"
+class Sted:
+    def __init__(self, ID, navn, gateadresse, postnr, poststed ):
+        self.id = ID
+        self.Tittel = navn
+        self.gateadresse = gateadresse
+        self.postnr = postnr
+        self.poststed = poststed
+
+    def __str__(self):
+        return f"ID: {self.id}, Navn: {self.Tittel}, Gateadresse: {self.gateadresse}, Postnr: {self.postnr}, Poststed: {self.poststed}"
