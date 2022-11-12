0bc2355aee8153bd26edd5c9b49e7c71c6559152
CHalexanderokokm <alexander.hvid@icloud.com>
2022-11-12 22:39:35 +0100

    ok

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ import klasser
 import Oving_10
 from datetime import datetime
 
+from meny import menycommon
+
 liste = list()
 test = "Avtale"
 
-print(Oving_10.lese_fil(liste,test))
+menycommon(test)
